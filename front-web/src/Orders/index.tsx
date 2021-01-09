8f68577f630620d10730d91c973f8115c53e6b4a
Vinicius Medeiros <vinicius_burzum@hotmail.com>
2021-01-09 17:12:23 -0300

    maps

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,28 @@
-import StepsHeader from './StepsHeader'
-import './styles.css'
+import { useEffect, useState } from 'react'; 
+import './styles.css';
+import ProductsList from './ProductsList';
+import StepsHeader from './StepsHeader';
+import { Product } from './Types';
+import { fetchProducts } from './api';
+import OrderLocation from './Orderlocation';
 
 
-function Orders(){
+
+function Orders() {
+      const [products,setProducts] = useState<Product[]>([]);
+        
+
+useEffect(() =>{ 
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error =>console.log(error))
+}, []);
+
     return (
         <div className="orders-container">
            <StepsHeader />
+           <ProductsList products={products} />
+           <OrderLocation />
         </div>
     )
 }
