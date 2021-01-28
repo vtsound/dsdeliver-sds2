2ece0b2ec82b54aa5c6b7fa559e1dda93d6f6ac4
Vinicius Medeiros <vinicius_burzum@hotmail.com>
2021-01-28 14:07:11 -0300

    Fix total

--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -8,7 +8,7 @@ export function fetchProducts(){
     return axios(`${API_URL}/products`)
 }
 export function fetchLocalMapBox(local: string){
-    return axios (`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+    return axios (`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`);
 }
 
 export function saveOrder(payload: OrderPayload){
